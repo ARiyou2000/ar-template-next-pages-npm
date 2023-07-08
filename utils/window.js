aab6c61a8654ef035e42dbd8e1573f41445a5cc8
ARiyou Jahan <AR.Jahan2000@gmail.com>
2023-07-08 16:38:45 +0330

    Add window and local storage utils

---
+++ utils/window.js
@@ -0,0 +1,28 @@
+let WINDOW = {};
+
+if (typeof window !== "undefined") {
+  // When code is on client-side. So we need to use actual methods and data.
+  WINDOW = window;
+} else {
+  // When code is on server-side.
+
+  // Other component are mostly server-side and need to match their logic and check their variable with other server-side components and logics.
+  // So following code will be used for them to pass the logic checking.
+  WINDOW = {
+    document: {
+      location: {},
+    },
+    localStorage: {
+      getItem: () => {
+      },
+      setItem: () => {
+      },
+      removeItem: () => {
+      },
+    },
+    addEventListener: () => {
+    },
+  };
+}
+
+export default WINDOW;
